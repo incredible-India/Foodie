65f994300adf452a4d93bf4c8531e5fce6904425
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2023-06-09 20:25:32 +0530

    removed swagger default one

---
+++ Foodie/Controllers/Authentication.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foodie.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class Authentication : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult signIn()
+        {
+            return Content("hello world");
+        }
+    }
+}
